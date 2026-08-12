b8a0f89db9c6b953290aea2e7f85192ce55e2fca
maaz2005-tech <maazzaki2005@gmail.com>
2026-08-12 15:51:55 +0530

    Time: 43 ms (12.84%) | Memory: 49.5 MB (17.78%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,21 @@
+class Solution {
+    int dp[];
+    boolean solve(int n){
+        if(n==0) return false;
+        
+        if(dp[n]!=-1) return dp[n]==1;
+        for(int i=1;i*i<=n;i++){
+            if(!solve(n-i*i)){
+                dp[n]=1;
+                return true;
+            }
+        }
+        dp[n]=0;
+        return false;
+    }
+    public boolean winnerSquareGame(int n) {
+        this.dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n);
+    }
+}
