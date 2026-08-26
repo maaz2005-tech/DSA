174e61064f6b04ba8b4d74c9176f12af712ea54a
maaz2005-tech <maazzaki2005@gmail.com>
2026-08-26 10:30:16 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (99.33%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,40 @@
+class Solution {
+    boolean check(String s,int sl,int l,int n){
+        while(l<n && sl<n){
+            if(s.charAt(l)==s.charAt(sl)){
+                l++;sl++;
+            }
+            else{
+                if((s.charAt(l)-'0')<(s.charAt(sl)-'0')){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+            return false;
+    }
+    public String shortestBeautifulSubstring(String s, int k) {
+        int n=s.length();
+        int l=0;
+        int sl=-1,sr=n;
+        int one=0;
+        for(int r=0;r<n;r++){
+            if(s.charAt(r)=='1') one++;
+            while(l<=r && (s.charAt(l)=='0'||one>k)){
+                if(s.charAt(l++)=='1'){
+                    one--;
+                } 
+            }
+            if(one==k){
+                if((sr-sl+1)>(r-l+1) ||((sr-sl+1)==(r-l+1)&&check(s,sl,l,n))){
+                    sl=l;sr=r;
+                }
+                
+            }
+        }
+        if(sl==-1) return "";
+        return s.substring(sl,sr+1);
+    }
+}
