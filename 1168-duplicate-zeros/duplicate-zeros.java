3edd086f037c7c1c70533c0c82eef8a1673b35d2
maaz2005-tech <maazzaki2005@gmail.com>
2026-09-21 10:58:28 +0530

    Time: 3 ms (38.75%) | Memory: 46.4 MB (98.33%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                q.add(0);
+                q.add(0);
+            }
+            else{
+                q.add(arr[i]);
+            }
+            arr[i]=q.remove();
+        }
+    }
+}
